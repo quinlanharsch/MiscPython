516102e59e25cca9732e4670853e3241859fbdcd
quinlanharsch <quinlanharsch@gmail.com>
2020-10-16 14:46:53 -0400

    candyTable

--- candyTable.py
+++ candyTable.py
@@ -1,67 +1,69 @@
 import numpy as np
 import itertools
 import sys
-np.set_printoptions(threshold=sys.maxsize)
+np.set_printoptions(threshold=255)
 
-dim = 4
+# =GLOBALS===============
+# (I'm not writing args for one int variable)
+dim = 3
+# Note: works only because the number of children = the number of candies taken
 
 
+# Note: the [1+Nd] means it's an Nd array with another dimension representing each possibility. TODO Space inefficient?
 def main():
     # [1+1d] child_pulls
-    # every possible combination of candy pulls
-    # works only because the number of children = the number of candies taken
+    # every possible combination of any given child's candy pulls
     child_pulls = [list(i) for i in itertools.combinations_with_replacement(list(range(dim)), dim)]
+    # TODO ** should be moved here in the iter..., too bad. I already got my
+    # TODO holy god, that's why it takes 3 hours for dim > 5
 
     # (iter) group_pulls & [1+2d] valid_group_pulls
-    # every possible combination of combination of candy pulls taking into account there can only be dim candies
+    # every possible combination of candy pulls with filter taking into account there can only be dim candies
     valid_group_pulls = []
     for group_pull in itertools.combinations_with_replacement(child_pulls, dim):
         group_pull = np.array(group_pull)
         count = np.bincount(group_pull.ravel())
+        # TODO **
         if all([True if c == dim else False for c in count]):  # each count == dim
             valid_group_pulls.append(group_pull)
-        # group_pull_counts.append(dict(zip(candy, count)))
     valid_group_pulls = np.array(valid_group_pulls)  # np array
 
     # [1+2d] valid_major_pulls
     # applies the constraint each child need a strict majority of some candy
     valid_major_pulls = []
     for valid_pull in valid_group_pulls:
-        # print("valid_pull: \n", valid_pull)  # TODO delete debugs
-        # [1+2d] counts_matrix
+
+        # [2d] counts_matrix
         # how many (val) of each candy (col) each child has (row)
         counts_matrix = np.apply_along_axis(np.bincount,
                                             axis=1,
                                             arr=valid_pull,
                                             minlength=np.max(valid_pull) + 1)
-        # print("counts_matrix: \n", counts_matrix)  # TODO
-        # [1+1d] counts_max
+
+        # [1d] counts_max
         # the candy(s) that child has most of; unique if valid 'major'
         # "must be strictly GREATER THAN (>, but unintentionally =) other max counts"
         counts_max = np.apply_along_axis(is_dist_max_fix,
                                          axis=0,  # cols / candy types
                                          arr=counts_matrix)
-        # print("counts_max: \n", counts_max)  # TODO
 
         # if theres one maxima for every child AND each child has a different max candy type
-        valid_total = -1 not in counts_max and len(set(counts_max)) == len(counts_max)
-        if valid_total:
+        if -1 not in counts_max and len(set(counts_max)) == len(counts_max):
             valid_major_pulls.append(valid_pull)
     valid_major_pulls = np.array(valid_major_pulls)
 
     # PRINT SOLUTION
     print('=VALID===================')
     print(valid_group_pulls)
-    print('=VALID=W/=MAJORITY=======')
+    print('\n=VALID=W/=MAJORITY=======')
     print(valid_major_pulls)
-    print('=FRACTION================')
-
+    print('\n=FRACTION================')
     print(len(valid_major_pulls), "/", len(valid_group_pulls))
 
 
 def is_dist_max_fix(arr):
-    n = list(np.argwhere(arr == np.amax(arr)).flatten())
-    return -1 if len(n) > 1 else n[0]
+    n = list(np.argwhere(arr == np.amax(arr)).flatten())  # loc of all instances of max
+    return -1 if len(n) > 1 else n[0]  # -1 if more than one location of max (not strictly >)
 
 
 if __name__ == '__main__':
